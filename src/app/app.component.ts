7901cd2af44df2033bea799593105ddda6470694
Carlos Barajas <suazo_es_kira@hotmail.com>
2021-06-04 18:01:37 -0500

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html'
+  //template : '<span>Carlos</span>'
+  //template: `
+  //<span>Hola de Nuevo</span>
+  //`
 })
 export class AppComponent {
-  title = 'bases';
+
+
 }
